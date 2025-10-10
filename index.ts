2a3f5e93dc53397cca722fb3f63a26b098e8549c
axtk <axtkx@inbox.ru>
2025-10-10 17:18:49 +0300

    lint

--- index.ts
+++ index.ts
@@ -4,36 +4,36 @@
  * - preserves templating characters without URL-encoding them.
  */
 export class QuasiURL {
-    hash: string;
-    hostname: string;
-    href: string;
-    origin: string;
-    pathname: string;
-    protocol: string;
-    search: string;
+  hash: string;
+  hostname: string;
+  href: string;
+  origin: string;
+  pathname: string;
+  protocol: string;
+  search: string;
 
-    constructor(url: string) {
-        let head = url.match(/^((\w+:)?\/\/([^/]+))(\/.*|$)/);
-        let tail = url.match(/(\?[^#]+)(#.+)?$/);
+  constructor(url: string) {
+    let head = url.match(/^((\w+:)?\/\/([^/]+))(\/.*|$)/);
+    let tail = url.match(/(\?[^#]+)(#.+)?$/);
 
-        let origin = head?.[1] ?? '';
-        let pathname = url
-            .replace(/^((\w+:)?\/\/[^/]+)/, '')
-            .replace(/\?.*$/, '')
-            .replace(/#.*$/, '');
-        let search = tail?.[1] ?? '';
-        let hash = tail?.[2] ?? '';
+    let origin = head?.[1] ?? "";
+    let pathname = url
+      .replace(/^((\w+:)?\/\/[^/]+)/, "")
+      .replace(/\?.*$/, "")
+      .replace(/#.*$/, "");
+    let search = tail?.[1] ?? "";
+    let hash = tail?.[2] ?? "";
 
-        this.origin = origin;
-        this.pathname = pathname;
-        this.search = search;
-        this.hash = hash;
+    this.origin = origin;
+    this.pathname = pathname;
+    this.search = search;
+    this.hash = hash;
 
-        this.href = `${origin}${pathname}${search}${hash}`;
-        this.hostname = head?.[3] ?? '';
-        this.protocol = head?.[2] ?? '';
-    }
-    toString() {
-        return this.href;
-    }
+    this.href = `${origin}${pathname}${search}${hash}`;
+    this.hostname = head?.[3] ?? "";
+    this.protocol = head?.[2] ?? "";
+  }
+  toString() {
+    return this.href;
+  }
 }
